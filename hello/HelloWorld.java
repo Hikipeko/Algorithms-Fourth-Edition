e36de1498eb3c99be7b741219a8cc233f83443ba
Hikipeko <1979160133@qq.com>
2021-04-06 11:44:40 +0800

    push

--- hello/HelloWorld.java
+++ hello/HelloWorld.java
@@ -1,34 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        int times = 1000000;
-        int length = 10;
-        int[] arr = new int[length];
-        int[][] result = new int[length][length];
+        String str1 = "like";
+        String str2 = str1.concat(" a dog.");
+        char ch = str1.charAt(1);
 
-        for (int i = 0; i < times; i++) {
-            for (int j = 0; j < length; j++)
-                arr[j] = j;
-            shuffle(arr);
-            for (int j = 0; j < length; j++) {
-                result[arr[j]][j]++;
-            }
-        }
-        for (int i = 0; i < length; i++) {
-            StdOut.printf("%3d appears ", i);
-            for (int j = 0; j < length; j++) {
-                StdOut.printf(" %8d ", result[i][j]);
-            }
-            StdOut.printf("\n");
-        }
+        StdOut.print(str1.getClass());
     }
 
-    public static void shuffle(int[] a) {
-        int N = a.length;
-        for (int i = 0; i < N; i++) {
-            int r = 0 + StdRandom.uniform(N - 0);
-            int temp = a[i];
-            a[i] = a[r];
-            a[r] = temp;
-        }
+    public static void increase(Counter a) {
+        a.increment();
     }
 }
